e98e830d2ff175dda255cd32f4128f7a8abc8039
Unoaccaso <62112172+Unoaccaso@users.noreply.github.com>
2022-05-10 12:30:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'freqle',         # How you named your package folder (MyLib)
-  packages = ['freqle'],   # Chose the same as "name"
-  version = '1.2.1',      # Start with a small number and increase it with every change you make
+  name = 'bosonCloud',         # How you named your package folder (MyLib)
+  packages = ['bosonCloud'],   # Chose the same as "name"
+  version = '2.0.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'The package is used to simulate the emission of gravitational waves from a cluster of black holes',   # Give a short description about your library
   author = 'Riccardo Felicetti',                   # Type in your name
   author_email = 'felicettiriccardo1@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/Unoaccaso/freqle',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Unoaccaso/freqle/archive/refs/tags/v_1.2.2.tar.gz',
+  url = 'https://github.com/Unoaccaso/bosonCloud',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/Unoaccaso/bosonCloud/archive/refs/tags/v_2.0.0.tar.gz',
   keywords = ['Gravitational Waves', 'Black Holes', 'Simulation'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
